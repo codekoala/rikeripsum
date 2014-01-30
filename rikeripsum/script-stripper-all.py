88bb8bcd9ff1ed5f1ef2e72ed23b30257641a2b9
Josh VanderLinden <codekoala@gmail.com>
2014-01-29 22:01:40 -0700

    Cleaning

--- rikeripsum/script-stripper-all.py
+++ rikeripsum/script-stripper-all.py
@@ -2,9 +2,15 @@
 # python script-stripper-all.py  1.18s user 0.02s system 99% cpu 1.208 total
 
 import os
-import pickle
+import sys
 
-num_words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven']
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle  # noqa
+
+
+NUM_WORDS = ('one', 'two', 'three', 'four', 'five', 'six', 'seven')
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,52 +23,52 @@ def main():
     lines = {}
 
     # check every script for all seasons
-    for season_num in range(1, 7):
-        season_dir = 'scripts/season%s' % num_words[season_num]
+    for season_num in NUM_WORDS:
+        season_dir = 'scripts/season%s' % season_num
         for script_file in os.listdir(season_dir):
             record = False
             dbuffer = ""
             charname = ""
-            f = open('scripts/season%s/%s' % (num_words[season_num], script_file))
 
-            # look for pattern indicating charector name
-            for data in f.readlines():
-                if "<p>" in data and "<br>" in data:
-                    splited = data.split("<p>")[-1].split("<br>")[0]
-                    splited = splited.split("'S")[0].split(" ")
+            with open('scripts/season%s/%s' % (season_num, script_file)) as f:
 
-                    # long list of words probably isnt a name
-                    if len(splited) > 5:
-                        continue
+                # look for pattern indicating charector name
+                for data in f:
+                    if "<p>" in data and "<br>" in data:
+                        splited = data.split("<p>")[-1].split("<br>")[0]
+                        splited = splited.split("'S")[0].split(" ")
 
-                    for name in splited:
-                        # charector names are uppercase
-                        if name.isupper():
-                            charname = name
-                            record = True
-                            dbuffer = ""
-                            break
+                        # long list of words probably isnt a name
+                        if len(splited) > 5:
+                            continue
 
-                # if name was just found, extract dialog until </p>
-                elif record:
-                    dbuffer += data
-                    if "</p>" in data:
-                        dbuffer = dbuffer.replace("<br>", "").replace("</p>", "")
-                        dbuffer = dbuffer.replace("\r", "").replace("\n", "")
-                        dbuffer = dbuffer.replace("\a", "").strip()
-                        dbuffer = ' '.join(dbuffer.split())
-                        line = {"text": dbuffer,
-                                "episode": script_file.replace('.htm', ''),
-                                "word_count": len(dbuffer.split())}
-                        record = False
-                        if charname in lines:
-                            lines[charname].append(line)
-                        else:
-                            lines[charname] = [line]
+                        for name in splited:
+                            # charector names are uppercase
+                            if name.isupper():
+                                charname = name
+                                record = True
+                                dbuffer = ""
+                                break
 
-    pickle_file = open(get_data('all_charectors.pickle'), 'wb')
-    pickle.dump(lines, pickle_file)
-    pickle_file.close()
+                    # if name was just found, extract dialog until </p>
+                    elif record:
+                        dbuffer += data
+                        if "</p>" in data:
+                            dbuffer = dbuffer.replace("<br>", "").replace("</p>", "")
+                            dbuffer = dbuffer.replace("\r", "").replace("\n", "")
+                            dbuffer = dbuffer.replace("\a", "").strip()
+                            dbuffer = ' '.join(dbuffer.split())
+                            line = {"text": dbuffer,
+                                    "episode": script_file.replace('.htm', ''),
+                                    "word_count": len(dbuffer.split())}
+                            record = False
+                            if charname in lines:
+                                lines[charname].append(line)
+                            else:
+                                lines[charname] = [line]
+
+    with open(get_data('all_characters.pickle'), 'wb') as pickle_file:
+        pickle.dump(lines, pickle_file)
 
     stats = []
     for name in lines:
@@ -70,7 +76,7 @@ def main():
     stats.sort()
 
     for num_lines, name in stats:
-        print(name, ": ", num_lines, "lines parsed")
+        sys.stdout.write('{}: {} lines parsed\n'.format(name, num_lines))
 
 
 if __name__ == '__main__':
